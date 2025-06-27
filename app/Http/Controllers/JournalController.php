ebf8e1f0efb1876b0e143b0ac520105a53d4f124
tansibMuttakin <tansib.muhaimin@gmail.com>
2025-06-27 12:54:11 +0600

    add try catch on the journal controller functions

--- app/Http/Controllers/JournalController.php
+++ app/Http/Controllers/JournalController.php
@@ -9,35 +9,45 @@ class JournalController extends Controller
 {
     public function index(Request $request)
     {
-        $query = Journal::with('sale')->latest();
+        try {
+            $query = Journal::with('sale')->latest();
 
-        if ($request->has('type')) {
-            $query->where('type', $request->type);
-        }
-
-        if ($request->has('from') && $request->has('to')) {
-            $query->whereBetween('created_at', [$request->from, $request->to]);
-        }
-
-        return $query->get();
-    }
-
-    public function summary(Request $request)
-    {
-        $types = ['sales', 'discount', 'vat', 'payment_cash', 'payment_due'];
-
-        $summary = [];
-
-        foreach ($types as $type) {
-            $query = Journal::where('type', $type);
+            if ($request->has('type')) {
+                $query->where('type', $request->type);
+            }
 
             if ($request->has('from') && $request->has('to')) {
                 $query->whereBetween('created_at', [$request->from, $request->to]);
             }
 
-            $summary[$type] = $query->sum('amount');
+            return $query->get();
+        } catch (\Throwable $th) {
+            return response()->json(['error' => $th->getMessage()], 500);
         }
 
-        return response()->json($summary);
+    }
+
+    public function summary(Request $request)
+    {
+        try {
+            $types = ['sales', 'discount', 'vat', 'payment_cash', 'payment_due'];
+
+            $summary = [];
+
+            foreach ($types as $type) {
+                $query = Journal::where('type', $type);
+
+                if ($request->has('from') && $request->has('to')) {
+                    $query->whereBetween('created_at', [$request->from, $request->to]);
+                }
+
+                $summary[$type] = $query->sum('amount');
+            }
+
+            return response()->json($summary);
+        } catch (\Throwable $th) {
+            return response()->json(['error' => $th->getMessage()], 500);
+        }
+
     }
 }
